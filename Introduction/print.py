cdae1607ec938fe97231aeff1e7de03cdb4ef8ce
aarika2948 <jalanaarika@gmail.com>
2026-06-03 15:43:50 +0530

    Introduction

---
+++ Introduction/print.py
@@ -0,0 +1,14 @@
+#my name
+print("My name is Aarika.")
+#my age
+print("I am 13 years old.")
+#my grade
+print("I am currently in grade 8.")
+#my school
+print("I study in Mayo College Girls' School, Ajmer.")
+#where i live?
+print("I live in Gorakhpur, Uttar Pradesh.")
+#numbers which are being added
+print("Numbers that are being added are 10 and 15")
+#their sum
+print("the sum of", 10, "+", 15, "=", 10+15,".")
