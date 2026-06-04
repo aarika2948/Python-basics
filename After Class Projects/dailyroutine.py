92dd2492f2f05183d1f8dc3ba5b4f4e3ec1ff2fb
aarika2948 <jalanaarika@gmail.com>
2026-06-04 15:13:05 +0530

    Introduction

---
+++ After Class Projects/dailyroutine.py
@@ -0,0 +1,34 @@
+#wake up time
+print("I wake in the morning at 5:45")
+#freshen up
+print("I freshen up for 10 minutes.")
+#sports
+print("I play tennis from 6 o'clock to 8 o'clock")
+#breakfast
+print("I come back home and eat my breakfast at 9 o'clock")
+#bath
+print("I take a bath.")
+#studies
+print("I sit for two hours and do my math work.")
+#fruits
+print("I take fruits and juices at noon.")
+#history class
+print("I attend my history tution from 1 o'clock to 2 o'clock")
+#lunch
+print("I have my lunch at 2:15 p.m.")
+#coding
+print("I attend my python class with codingal")
+#homework
+print("I do the ptojects of my coding class from 4 o'clock to 4:30.")
+#snacks
+print("I take my snacks and watch TV.")
+#English
+print("I attend my english tution from 5 o'clock to 6 o'clock")
+#outing
+print("I go out with my parents and siblings.")
+#studies
+print("I sit for two hours and do studies.")
+#dinner
+print("I eat my dinner at 9:30.")
+#sleep
+print("I go to sleep at 11 o'clock.")
