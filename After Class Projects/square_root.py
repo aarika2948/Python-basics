289cde69ea52c0fb29c94ba8f8ae6f5dc031a25f
aarika2948 <jalanaarika@gmail.com>
2026-06-08 12:54:51 +0530

    Square Root

---
+++ After Class Projects/square_root.py
@@ -0,0 +1,7 @@
+import math
+#Take the input of the base from the user
+x=float(input("Enter the value of which you want the square root="))
+#Calcuate the square root via the root function
+root=math.sqrt(x)
+#Displaying the square root
+print(f"The square root of {x} is {root}")
