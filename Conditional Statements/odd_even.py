4aac3304da1f27a90e22d022b90596267a5a3b03
aarika2948 <jalanaarika@gmail.com>
2026-06-09 12:23:29 +0530

    Conditional Statements

---
+++ Conditional Statements/odd_even.py
@@ -0,0 +1,8 @@
+#Asking the user for the value
+x = int(input("Enter the value="))
+#Giving the condition
+if (x%2==0):
+    print(f"{x} is an even no.")
+else:
+    print(f"{x} is an odd no.")
+    
